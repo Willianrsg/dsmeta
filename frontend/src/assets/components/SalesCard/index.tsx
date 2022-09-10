9d0582645526c05a6db6a9a8864409b2fecd0bfa
willian rafael <willian.rafael18@gmail.com>
2022-09-10 18:00:05 -0300

    Axios, useEffect first request

--- frontend/src/assets/components/SalesCard/index.tsx
+++ frontend/src/assets/components/SalesCard/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton';
@@ -8,8 +9,15 @@ function SalesCard() {
     //Declarando um dado e a funçao, definindo o useDate
     const min = new Date(new Date().setDate(new Date().getDate() - 365));//pegando a data de 1 ano atras
     const max = new Date();
-    const [minDate, setMinDate]= useState(min);
+    const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales")
+            .then(response => {
+                console.log(response.data);
+            })
+    }, []);
 
     return (
         <div className="dsmeta-card">
